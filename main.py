ec9da3788fc095a1ece120e34c25de657c089f60
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2025-09-02 09:51:57 -0500

    Complete 2a.

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ See problemset-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        x, y = min(a, b), max(a, b)
+        return foo(x, y, % x)
 
 def longest_run(mylist, key):
     ### TODO
